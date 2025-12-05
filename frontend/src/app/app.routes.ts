06fd25dd214ddb2716619e89cc0b4eae7928be9e
zaid03 <terguyzaid@gmail.com>
2025-12-05 13:35:01 +0100

    updated the front for familias only

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,22 +3,16 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { EntComponent } from './ent/ent.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProveedoreesComponent } from './proveedorees/proveedorees.component';
 import { CentrogestorComponent } from './centrogestor/centrogestor.component';
 import { EjeComponent } from './eje/eje.component';
-import { FacturasComponent } from './facturas/facturas.component';
-import { CreditoComponent } from './credito/credito.component';
 import { FamiliaComponent } from './familia/familia.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'ent', component: EntComponent },
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'proveedorees', component: ProveedoreesComponent },
     { path: 'eje', component: EjeComponent},
     { path: 'centro-gestor', component: CentrogestorComponent},
-    { path: 'facturas', component: FacturasComponent},
-    { path: 'credito', component: CreditoComponent},
     { path: 'familia', component: FamiliaComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' }, //route by default
 ];
